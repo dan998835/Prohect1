630402ab20c712e111449c0d3139b68accd84077
dan998835 <101984108+dan998835@users.noreply.github.com>
2022-03-20 19:20:54 +0200

    Update zip_job.py

--- zip_job.py
+++ zip_job.py
@@ -1,13 +1,29 @@
+import os
 from os.path import exists
 import sys
+from zipfile import Zipfile
 
-array=['a','b','b','d']
-for x in array:
-  f=open(x + ".txt")
-  
-for x in array:
-  if (os.path.exists(x + ".txt") != true):
-    print('file doesnt exist - quit script')
-    sys.exit(1)
+def main():
+  #a
+  array=['a','b','b','d']
+  #b
+  for x in array:
+    f=open(x + ".txt")
+    f.close()
+  #c
+  for x in array:
+    if (os.path.exists(x + ".txt") != true):
+      print('file doesnt exist - quit script')
+      sys.exit(1)
+  #d
+  for x in array:
+    zip_file = ZipFile(x + '_' + ENV VERSION + '.zip', 'w')
+    zip_file.write(x + '.txt')
+    zip_file.close()
+  #e 
+  for x in array:
+    if (os.path.exists(x + '_' + ENV VERSION + ".zip") != true):
+      print('file doesnt exist - quit script')
+      sys.exit(1)
     
       
